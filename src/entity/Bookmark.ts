3fbc5ae766fa1b7bace9e968c3f1c0acb1e15715
Joseph Furlott <jmfurlott@gmail.com>
2019-04-25 08:44:26 -0400

    Initial commit

---
+++ src/entity/Bookmark.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column, } from "typeorm";
+
+@Entity()
+export class Bookmark {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column("varchar")
+  description: string;
+
+  @Column("varchar")
+  href: string;
+}
---
+++ src/entity/User.ts
@@ -0,0 +1,17 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class User {
+
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ unique: true })
+  github_id: string; // NOTE only going to allow Github authentication
+}
