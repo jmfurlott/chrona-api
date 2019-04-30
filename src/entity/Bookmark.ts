f0566a571a349eb1381b8eeb100b320debf08cc6
Joseph Furlott <jmfurlott@gmail.com>
2019-04-29 21:25:59 -0400

    ENH: adds archived, created_at, and updated_at columns

--- src/entity/Bookmark.ts
+++ src/entity/Bookmark.ts
@@ -1,9 +1,18 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, Column } from "typeorm";
 
 @Entity()
 export class Bookmark {
+  @Column({ default: false })
+  archived: boolean;
+
   @PrimaryGeneratedColumn("uuid")
   id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 
   @Column()
   title: string;
