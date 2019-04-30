5715a87fc7fce6a4ed5efd5105b9ed0ebe2bd7d4
Joseph Furlott <jmfurlott@gmail.com>
2019-04-30 09:13:34 -0400

    ENH: Copies /users* routes into /bookmarks*

---
+++ src/routes/bookmarks.ts
@@ -0,0 +1,70 @@
+import { Router } from "express";
+import * as express from "express";
+import { getRepository } from "typeorm";
+
+import { Bookmark } from "../entity/Bookmark";
+
+export default (): Router => {
+  const router = express.Router();
+  const bookmarkRepository = getRepository(Bookmark);
+
+  const checkBookmarkExists = async (req, res, next): Promise<void> => {
+    try {
+      if (
+        (await bookmarkRepository.count({ id: req.params.id, archived: false })) ===
+          0
+      ) {
+        res.sendStatus(404);
+      }
+      next();
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  router.get("/bookmarks", async (req, res, next) => {
+    try {
+      res.json(await bookmarkRepository.find());
+    } catch (e) {
+      next(e);
+    }
+  });
+
+  router.get("/bookmarks/:id", checkBookmarkExists, async (req, res, next) => {
+    try {
+      res.json(
+        await bookmarkRepository.findOne({ id: req.params.id, archived: false }),
+      );
+    } catch (e) {
+      next(e);
+    }
+  });
+
+  router.post("/bookmarks", async (req, res, next) => {
+    try {
+      res.json(await bookmarkRepository.save(req.body));
+    } catch (e) {
+      next(e);
+    }
+  });
+
+  router.put("/bookmarks/:id", checkBookmarkExists, async (req, res, next) => {
+    try {
+      await bookmarkRepository.update(req.params.id, req.body);
+      res.json(await bookmarkRepository.findOne(req.params.id));
+    } catch (e) {
+      next(e);
+    }
+  });
+
+  router.delete("/bookmarks/:id", checkBookmarkExists, async (req, res, next) => {
+    try {
+      await bookmarkRepository.update(req.params.id, { archived: true });
+      res.sendStatus(204);
+    } catch (e) {
+      next(e);
+    }
+  });
+
+  return router;
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import * as express from "express";
 
 import users from "./users";
+import bookmarks from "./bookmarks";
 
 export default (): Router => {
   const router = express.Router();
 
+  router.use(bookmarks());
   router.use(users());
 
   return router;
