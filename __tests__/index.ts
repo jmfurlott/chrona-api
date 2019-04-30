1e9fde508aa01d80b8edcd5ea12cb7ece6632ecc
Joseph Furlott <jmfurlott@gmail.com>
2019-04-29 21:17:40 -0400

    TST: Adds CRU(not D) tests for /users/*
    NOTE: POST route does not exist so failing here
    NOTE: Waiting on the delete routes because need to add archival

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,8 +1,10 @@
 import axiosist from "axiosist";
+import * as faker from "faker";
 import { createConnection } from "typeorm";
 
 import makeApp from "../src/app";
 import { testFactory } from "./utils";
+import { User } from "../src/entity/User";
 
 
 let connection;
@@ -19,6 +21,15 @@ test('Establishes connection and starts app', async (): Promise<any> => {
   expect(res.status).toBe(200);
 });
 
+test('GET /users returns expected count', async (): Promise<any> => {
+  const app = makeApp();
+
+  const expectedCount: number = await connection.getRepository(User).count();
+  const res = await axiosist(app).get(`/users`);
+  expect(res.status).toBe(200);
+  expect(res.data.length).toBe(expectedCount);
+});
+
 
 test('GET /users/:id returns expected user', async (): Promise<any> => {
   const app = makeApp();
@@ -26,4 +37,37 @@ test('GET /users/:id returns expected user', async (): Promise<any> => {
   const res = await axiosist(app).get(`/users/${ user.id }`);
   expect(res.status).toBe(200);
   expect(res.data.id).toBe(user.id);
+});
+
+
+test('PUT /users/:id updates and returns expected user', async (): Promise<any> => {
+  const app = makeApp();
+  const { user } = await testFactory(connection);
+
+  const newData = {
+    name: faker.name.findName(),
+  };
+
+  const res = await axiosist(app).put(`/users/${ user.id }`, newData);
+  expect(res.status).toBe(200);
+  expect(res.data.id).toBe(user.id);
+  expect(res.data.name).toBe(newData.name);
+});
+
+
+test('POST /users creates and returns expected user', async (): Promise<any> => {
+  const app = makeApp();
+
+  const newUser = {
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    github_id: faker.random.uuid(),
+  };
+
+  const res = await axiosist(app).post(`/users`, newUser);
+
+  expect(res.status).toBe(200);
+  expect(res.data.name).toBe(newUser.name);
+  expect(res.data.email).toBe(newUser.email);
+  expect(res.data.github_id).toBe(newUser.github_id);
 });
