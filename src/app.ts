93c52fb15eb2e05494afc66ed487019d46914913
Joseph Furlott <jmfurlott@gmail.com>
2019-04-30 08:42:32 -0400

    ENH: Re-wrote all user routes for full CRUD

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,9 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as cors from "cors";
-
 import { Application, Request, Response } from "express";
 
-import { Routes } from "./routes";
+import routes from "./routes";
 
 
 export default (): Application => {
@@ -13,18 +12,7 @@ export default (): Application => {
   app.use(bodyParser.json());
   app.use(cors());
 
-  // register express routes from defined application routes
-  Routes.forEach(route => {
-    (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-      const result = (new (route.controller as any))[route.action](req, res, next);
-      if (result instanceof Promise) {
-        result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
-
-      } else if (result !== null && result !== undefined) {
-        res.json(result);
-      }
-    });
-  });
+  app.use(routes());
 
   return app;
 };
--- src/routes.ts
+++ src/routes.ts
@@ -1,23 +1,57 @@
-import {UserController} from "./controller/UserController";
+import { Router } from "express";
+import * as express from "express";
+import { getRepository } from "typeorm";
 
-export const Routes = [{
-    method: "get",
-    route: "/users",
-    controller: UserController,
-    action: "all"
-}, {
-    method: "get",
-    route: "/users/:id",
-    controller: UserController,
-    action: "one"
-}, {
-    method: "post",
-    route: "/users",
-    controller: UserController,
-    action: "save"
-}, {
-    method: "delete",
-    route: "/users/:id",
-    controller: UserController,
-    action: "remove"
-}];
+import { User } from "./entity/User";
+
+export default (): Router => {
+  const router = express.Router();
+  const userRepository = getRepository(User);
+
+  router.get("/users", async (req, res, next) => {
+    try {
+      res.json(await userRepository.find());
+    } catch (e) {
+      next(e);
+    }
+  });
+
+  router.get("/users/:id", async (req, res, next) => {
+    try {
+      if (await userRepository.count({ id: req.params.id, archived: false }) === 0) {
+        res.sendStatus(404);
+      }
+      res.json(await userRepository.findOne({ id: req.params.id, archived: false }));
+    } catch (e) {
+      next(e);
+    }
+  });
+
+  router.post("/users", async (req, res, next) => {
+    try {
+      res.json(await userRepository.save(req.body));
+    } catch (e) {
+      next(e);
+    }
+  });
+
+  router.put("/users/:id", async (req, res, next) => {
+    try {
+      await userRepository.update(req.params.id, req.body);
+      res.json(await userRepository.findOne(req.params.id));
+    } catch (e) {
+      next(e);
+    }
+  });
+
+  router.delete("/users/:id", async (req, res, next) => {
+    try {
+      await userRepository.update(req.params.id, { archived: true });
+      res.sendStatus(204);
+    } catch (e) {
+      next(e);
+    }
+  });
+
+  return router;
+};
