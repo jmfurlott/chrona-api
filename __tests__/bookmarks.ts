340b8d1b280461bd557b5ab2d739637d69df6a37
Joseph Furlott <jmfurlott@gmail.com>
2019-05-09 17:15:51 -0400

    TST: Updated tests to work against secure routes

--- __tests__/bookmarks.ts
+++ __tests__/bookmarks.ts
@@ -1,4 +1,7 @@
-import axiosist from "axiosist";
+import * as axiosist from "axiosist";
+import axios from "axios";
+import { AxiosInstance } from "axios";
+import { Connection } from "typeorm";
 import * as faker from "faker";
 
 import createConnection from "../src/connection";
@@ -7,9 +10,24 @@ import { testFactory } from "./utils";
 import { Bookmark } from "../src/entity/Bookmark";
 
 
-let connection;
+let connection: Connection;
+let userRequest: AxiosInstance;
 beforeAll(async () => {
   connection = await createConnection();
+
+  // Login and generate a token
+  const app = makeApp();
+  const { data: { token } } = await axiosist(app).post("/auth/local/signup", {
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  });
+
+  userRequest = axios.create({
+    adapter: axiosist.createAdapter(app),
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 });
 
 afterAll(() => connection.close());
@@ -21,33 +39,28 @@ test('GET /bookmarks unauthenticated returns a 401', async (): Promise<any> => {
 });
 
 test('GET /bookmarks returns expected count', async (): Promise<any> => {
-  const app = makeApp();
-
   const expectedCount: number = await connection.getRepository(Bookmark).count({ archived: false });
-  const res = await axiosist(app).get(`/bookmarks`);
+  const res = await userRequest.get(`/bookmarks`);
   expect(res.status).toBe(200);
   expect(res.data.length).toBe(expectedCount);
 });
 
-
 test('GET /bookmarks/:id returns expected bookmark', async (): Promise<any> => {
-  const app = makeApp();
   const { bookmark } = await testFactory();
-  const res = await axiosist(app).get(`/bookmarks/${bookmark.id}`);
+  const res = await userRequest.get(`/bookmarks/${bookmark.id}`);
   expect(res.status).toBe(200);
   expect(res.data.id).toBe(bookmark.id);
 });
 
 
 test('PUT /bookmarks/:id updates and returns expected bookmark', async (): Promise<any> => {
-  const app = makeApp();
   const { bookmark } = await testFactory();
 
   const newData = {
     title: faker.lorem.words(),
   };
 
-  const res = await axiosist(app).put(`/bookmarks/${bookmark.id}`, newData);
+  const res = await userRequest.put(`/bookmarks/${bookmark.id}`, newData);
   expect(res.status).toBe(200);
   expect(res.data.id).toBe(bookmark.id);
   expect(res.data.title).toBe(newData.title);
@@ -55,15 +68,13 @@ test('PUT /bookmarks/:id updates and returns expected bookmark', async (): Promi
 
 
 test('POST /bookmarks creates and returns expected bookmark', async (): Promise<any> => {
-  const app = makeApp();
-
   const newBookmark = {
     title: faker.lorem.words(),
     description: faker.lorem.sentences(),
     href: faker.internet.url(),
   };
 
-  const res = await axiosist(app).post(`/bookmarks`, newBookmark);
+  const res = await userRequest.post(`/bookmarks`, newBookmark);
 
   expect(res.status).toBe(200);
   expect(res.data.title).toBe(newBookmark.title);
@@ -73,14 +84,16 @@ test('POST /bookmarks creates and returns expected bookmark', async (): Promise<
 
 
 test('DELETE /bookmarks/:id updates and returns 204', async (): Promise<any> => {
-  const app = makeApp();
   const { bookmark } = await testFactory();
 
   // The actual delete returns a 204
-  let res = await axiosist(app).delete(`/bookmarks/${bookmark.id}`);
+  let res = await userRequest.delete(`/bookmarks/${bookmark.id}`);
   expect(res.status).toBe(204);
 
-  // Show now 404
-  res = await axiosist(app).get(`/bookmarks/${bookmark.id}`);
-  expect(res.status).toBe(404);
+  // So, now 404
+  try {
+    await userRequest.get(`/bookmarks/${bookmark.id}`);
+  } catch ({ response: { status } }) {
+    expect(status).toBe(404);
+  }
 });
