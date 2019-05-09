dafe229162fd12ac0f9af1c4f9bdc5f239efaf84
Joseph Furlott <jmfurlott@gmail.com>
2019-05-08 20:46:37 -0400

    TST: Refactored test utils to use repositories

--- __tests__/utils.ts
+++ __tests__/utils.ts
@@ -1,28 +1,28 @@
 import * as faker from "faker";
+import { getRepository } from "typeorm";
 
 import { User } from "../src/entity/User";
 import { Bookmark } from "../src/entity/Bookmark";
 // Testing utils
 
 // Test seeder
-export const testFactory = async (connection) => {
-  const user = await connection.manager.save(connection.manager.create(User, {
+export const testFactory = async () => {
+  const userRepository = getRepository(User);
+  const user = await userRepository.save({
     name: faker.name.findName(),
     email: faker.internet.email(),
     github_id: faker.random.uuid(),
-  }));
+  });
 
-
-  const bookmark = await connection.manager.save(connection.manager.create(Bookmark, {
+  const bookmarkRepository = getRepository(Bookmark);
+  const bookmark = await bookmarkRepository.save({
     title: faker.lorem.words(),
     description: faker.lorem.sentences(),
     href: faker.internet.url(),
-  }));
-
+  });
 
   return {
     bookmark,
     user
   };
 };
-
